9cb171d86768c11900bf88d7693febc57b74cf8c
Dixon <dixonwillow23@gmail.com>
2023-10-27 19:54:42 +0700

    feat: login page finishing

--- Urbane/resources/views/pages/login.blade.php
+++ Urbane/resources/views/pages/login.blade.php
@@ -1,29 +1,31 @@
 @extends('template.authTemplate')
 
 @section('body')
-    <div class="font-urbanist h-screen w-full">
-        <div class="flex justify-center items-center w-full h-full">
-            <div class="max-w-620 h-auto border-solid bg-bgcolor">
-                <div>
-                    <h1 class="text-3xl text-primary font-bold">Login</h1>
-                    <h2 class="text-xl text-secondary font-medium">Welcome back Urbanist</h2>
-                </div>
+    <div class="flex flex-col justify-center items-start w-full h-full p-4">
+        
+            <div>
+                <h1 class="text-3xl text-primary font-bold">Login</h1>
+                <h2 class="text-xl text-secondary font-medium">Welcome back Urbanist</h2>
+            </div>
 
-                <form action="" class="flex flex-col justify-center items-center gap-8">
-                    <div class="flex justify-start items-center border-b-2">
-                        <i></i>
-                        <input type="text" name="email" id="emailInput">
-                    </div>
-                    <div class="flex justify-start items-center border-b-2">
-                        <i></i>
-                        <input type="text" name="password" id="passwordInput">
-                    </div>
-                </form>
-
-                <div class="flex flex-col justify-center items-center">
-                    <button class="text-2xl font-bold text-highlight bg-primary w-[90%] rounded-lg">Login</button>
-                    <p>Don't have an account ? <span><a href="">Register here</a></span></p>
+        <div class="flex flex-col w-full">
+            <form action="" method="POST" class="flex flex-col justify-center items-center gap-8 py-8">
+                <div class="flex justify-start items-center border-b-2 py-2 border-stroke w-full">
+                    <i class="bx bxs-envelope bx-sm pl-1 pr-2 text-secondary"></i>
+                    <input class="border-l-2 border-stroke bg-transparent w-full text-secondary placeholder-secondary pl-2 focus:outline-none" type="text" name="email" id="emailInput" placeholder="Email">
                 </div>
+                <div class="flex flex-col justify-center items-end w-full">
+                    <div class="flex justify-start items-center border-b-2 py-2 border-stroke w-full">
+                        <i class="bx bxs-lock bx-sm pl-1 pr-2 text-secondary"></i>
+                        <input class="border-l-2 border-stroke bg-transparent w-full text-secondary placeholder-secondary pl-2 focus:outline-none" type="password" name="password" id="passwordInput" placeholder="Password">
+                    </div>
+                    <a class="font-semibold" href="#">Forgot password?</a>
+                </div>
+            </form>
+
+            <div class="flex flex-col justify-center items-center">
+                <button class="text-2xl font-bold text-highlight bg-primary w-full rounded-lg py-2">Login</button>
+                <p>Don't have an account ? <span class="text-primary font-semibold"><a href="">Register here</a></span></p>
             </div>
         </div>
     </div>
--- Urbane/resources/views/template/authTemplate.blade.php
+++ Urbane/resources/views/template/authTemplate.blade.php
@@ -8,7 +8,7 @@
             class="w-full h-full">
         <img src="{{ url('assets/Logo.png')}}" alt="" class="w-24 h-16 absolute top-0 mt-12 right-0 mr-12 hidden min-[500px]:block">
 
-        <div class="absolute h-screen min-[500px]:h-auto top-0 min-[500px]:top-[50%] md:left-[5%] min-[500px]:left-[50%] min-[500px]:translate-x-[-50%] md:translate-x-0 min-[500px]:translate-y-[-50%] md:translate-y-0 p-6 rounded-md font-urbanist min-[500px]:max-w-[600px] w-full backdrop-blur-xl bg-white/50">
+        <div class="absolute h-screen min-[500px]:h-auto top-0 min-[500px]:top-[50%] md:left-[5%] min-[500px]:left-[50%] min-[500px]:translate-x-[-50%] md:translate-x-0 min-[500px]:translate-y-[-50%] p-6 rounded-md font-urbanist min-[500px]:max-w-[600px] w-full backdrop-blur-xl bg-white/50">
             @yield('body')
         </div>
     </body>
