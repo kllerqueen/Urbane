f8de3a819fe3143d4502fe55eb649a8ea46d84d5
Dixon <dixonwillow23@gmail.com>
2023-10-27 16:21:32 +0700

    config: folders config

---
+++ Urbane/resources/views/template/template.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    @vite(['resources/css/app.css'])
+
+    <title>Urbane</title>
+
+    @yield('head')
+</head>
+
+<body>
+    @yield('body')
+
+    <h1>Test</h1>
+    
+</body>
+
+</html>
