c19db42f3c19516c1a5b776064e424eba4ec73c4
wete <williamtan1232@gmail.com>
2023-12-08 23:03:27 +0700

    media page

--- Urbane/resources/views/pages/media.blade.php
+++ Urbane/resources/views/pages/media.blade.php
@@ -1,18 +1,30 @@
 @extends('template.template')
 
 @section('body')
-    <div class="flex flex-col justify-center items-center">
+    <div class="flex flex-col justify-center items-center px-5">
         <h1 class="bold-64 text-primary">MEDIA</h1>
-
-        <div class="flex flex-row overflow-x-auto whitespace-nowrap">
-            <div class="top-45 w-full h-20 absolute bg-white rounded-[500px]"></div>
-            <img class="py-10 pr-10" src="{{ url('assets/media/ScrollingImage1.png') }}" alt="/">
-            <img class="p-10" src="{{ url('assets/media/ScrollingImage2.png') }}" alt="/">
-            <img class="p-10" src="{{ url('assets/media/ScrollingImage3.png') }}" alt="/">
-            <img class="p-10" src="{{ url('assets/media/ScrollingImage4.png') }}" alt="/">
-            <img class="p-10" src="{{ url('assets/media/ScrollingImage5.png') }}" alt="/">
-            <img class="p-10" src="{{ url('assets/media/ScrollingImage6.png') }}" alt="/">
-            <img class="py-10 pl-10" src="{{ url('assets/media/ScrollingImage7.png') }}" alt="/">
+        <div id="slider-box" class='relative flex items-center w-full h-full whitespace-nowrap overflow-y-hidden overflow-x-auto my-12 gap-4 ' >
+                <div id="slide"  class=" relative flex flex-col bg-white/20 text-white min-w-[140px] min-[500px]:min-w-[200px] lg:min-w-[250px] min-h-[160px] md:min-h-[240px] lg:min-h-[300px] ">
+                    <img src="{{ url('assets/media/ScrollingImage1.png') }}" alt=""  class='w-full h-[150px] md:h-[250px]'/>
+                </div>
+                <div id="slide"  class=" relative flex flex-col bg-white/20 text-white min-w-[140px] min-[500px]:min-w-[200px] lg:min-w-[250px] min-h-[160px] md:min-h-[240px] lg:min-h-[300px] ">
+                    <img src="{{ url('assets/media/ScrollingImage2.png') }}" alt=""  class='w-full h-[150px] md:h-[250px]'/>
+                </div>
+                <div id="slide"  class=" relative flex flex-col bg-white/20 text-white min-w-[140px] min-[500px]:min-w-[200px] lg:min-w-[250px] min-h-[160px] md:min-h-[240px] lg:min-h-[300px] ">
+                    <img src="{{ url('assets/media/ScrollingImage3.png') }}" alt=""  class='w-full h-[150px] md:h-[250px]'/>
+                </div>
+                <div id="slide"  class=" relative flex flex-col bg-white/20 text-white min-w-[140px] min-[500px]:min-w-[200px] lg:min-w-[250px] min-h-[160px] md:min-h-[240px] lg:min-h-[300px] ">
+                    <img src="{{ url('assets/media/ScrollingImage4.png') }}" alt=""  class='w-full h-[150px] md:h-[250px]'/>
+                </div>
+                <div id="slide"  class=" relative flex flex-col bg-white/20 text-white min-w-[140px] min-[500px]:min-w-[200px] lg:min-w-[250px] min-h-[160px] md:min-h-[240px] lg:min-h-[300px] ">
+                    <img src="{{ url('assets/media/ScrollingImage5.png') }}" alt=""  class='w-full h-[150px] md:h-[250px]'/>
+                </div>
+                <div id="slide"  class=" relative flex flex-col bg-white/20 text-white min-w-[140px] min-[500px]:min-w-[200px] lg:min-w-[250px] min-h-[160px] md:min-h-[240px] lg:min-h-[300px] ">
+                    <img src="{{ url('assets/media/ScrollingImage6.png') }}" alt=""  class='w-full h-[150px] md:h-[250px]'/>
+                </div>
+                <div id="slide"  class=" relative flex flex-col bg-white/20 text-white min-w-[140px] min-[500px]:min-w-[200px] lg:min-w-[250px] min-h-[160px] md:min-h-[240px] lg:min-h-[300px] ">
+                    <img src="{{ url('assets/media/ScrollingImage7.png') }}" alt=""  class='w-full h-[150px] md:h-[250px]'/>
+                </div>
         </div>
     </div>
 @endsection
