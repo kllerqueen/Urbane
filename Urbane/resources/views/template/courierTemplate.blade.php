8c9b5ba77fd2635362d95e0a50f1de18df8f402b
wete <williamtan1232@gmail.com>
2023-12-09 12:31:57 +0700

    added courier sidebar

---
+++ Urbane/resources/views/template/courierTemplate.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    @include('partials.head')
+
+    <body class="w-full font-urbanist flex relative bg-black">
+        @include('partials.courierSidebar')
+        @yield('body')
+    </body>
+</html>
