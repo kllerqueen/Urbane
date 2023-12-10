9fb8fb441d3057fd0a15238b62acb3e41b3c90bf
wete <williamtan1232@gmail.com>
2023-12-10 17:04:30 +0700

    fix border error

--- Urbane/resources/views/pages/login.blade.php
+++ Urbane/resources/views/pages/login.blade.php
@@ -25,7 +25,7 @@
             <form action="{{ route('login') }}" method="POST" class="flex flex-col justify-center gap-8 py-8">
                 @csrf
                 <div>
-                    <div class="flex justify-start items-center border-b-2 py-2 border-stroke w-full @error('email') border-red-500 @enderror">
+                    <div class="flex justify-start items-center border-b-2 py-2 border-stroke w-full @error('email') !border-red-500 @enderror">
                         <i class="bx bxs-envelope bx-sm pl-1 pr-2 text-secondary"></i>
                         <input class="input border-l-2 border-stroke bg-transparent w-full text-secondary placeholder-secondary pl-2 focus:outline-none @error('email') is-invalid @enderror" type="email" name="email" id="emailInput" placeholder="Email" value="{{ old('email') }}">
                     </div>
@@ -35,7 +35,7 @@
                 </div> 
 
                 <div>
-                    <div class="flex justify-start items-center border-b-2 py-2 border-stroke w-full @error('password') border-red-500 @enderror">
+                    <div class="flex justify-start items-center border-b-2 py-2 border-stroke w-full @error('password') !border-red-500 @enderror">
                         <i class="bx bxs-lock bx-sm pl-1 pr-2 text-secondary"></i>
                         <input class="input border-l-2 border-stroke bg-transparent w-full text-secondary placeholder-secondary pl-2 focus:outline-none @error('password') is-invalid @enderror" type="password" name="password" id="passwordInput" placeholder="Password">
                         <ion-icon name="eye-outline" class="text-[30px] mr-2 text-secondary " id="eye-open" onclick="hidePassword()"></ion-icon>
